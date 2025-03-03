4d89bd3e8cec71fad490b8e594996f95cc17b474
sjtuross <sjtuross@msn.com>
2025-03-03 22:12:25 +0800

    bump version

--- StrmAssistant/Options/ModOptions.cs
+++ StrmAssistant/Options/ModOptions.cs
@@ -71,7 +71,7 @@ namespace StrmAssistant.Options
             (Plugin.Instance.ApplicationHost.ApplicationVersion >= new Version("4.8.3.0") &&
              Plugin.Instance.ApplicationHost.ApplicationVersion < new Version("4.9.0.0") ||
              Plugin.Instance.ApplicationHost.ApplicationVersion >= new Version("4.9.0.15") &&
-             Plugin.Instance.ApplicationHost.ApplicationVersion <= new Version("4.9.0.40"));
+             Plugin.Instance.ApplicationHost.ApplicationVersion <= new Version("4.9.0.41"));
 
         public void Initialize()
         {
